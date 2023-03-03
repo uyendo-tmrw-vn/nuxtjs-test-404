4a30baac60acae187c68d857d1d13d99c2eea48c
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-03-03 11:41:48 +0700

    3

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,6 +1,23 @@
-import type { RouterOptions } from '@nuxt/schema'
 
+import type { RouterConfig } from '@nuxt/schema'
 // https://router.vuejs.org/api/interfaces/routeroptions.html
-export default <RouterOptions> {
-	
+export default <RouterConfig> {
+  routes: (_routes) => [
+    {
+      name: 'home',
+      path: '/',
+      component: () => import('~/pages/index.vue')
+    },
+    {
+      name: 'news',
+      path: '/news',
+      component: () => import('~/pages/news.vue')
+    },
+    {
+      name: '404',
+      path: '*',
+      component: () => import('~/pages/404.vue')
+    },
+    // { path: '*', beforeEnter: (to, from, next) => { next('/404') } },
+  ],
 }
--- pages/404.vue
+++ pages/404.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="mt-7 max-w-sm mx-auto text-center card">
-    <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
+    <h1>Page Not Found</h1>
+    <!-- <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
     <p class="mt-7 text-6xl">Ooops.</p>
     <p class="mt-7">{{error.message}}</p>
-    <button class="btn my-7" @click="handleError">Go Home...</button>
+    <button class="btn my-7" @click="handleError">Go Home...</button> -->
   </div>
 </template>
 
