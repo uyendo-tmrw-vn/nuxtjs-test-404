97a09b9f00d293aaf2e84fbe7e4cccc01badfa72
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-03-03 17:02:59 +0700

    test

--- plugins/error.js
+++ plugins/error.js
@@ -9,6 +9,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     console.log('app:error')
     if (process.client) {
       console.log(_args&&_args[0].statusCode,_args[0])
+      window.location.href('/error404.html')
     }
   })
   nuxtApp.vueApp.config.errorHandler = (..._args) => {
