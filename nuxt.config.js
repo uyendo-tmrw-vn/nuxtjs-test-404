bf4aef50ed4d506f98b4c4c341cb0cbbb17bf862
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-03-03 16:20:09 +0700

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,4 +5,15 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  "target": "server",
+	"nitro": {
+		"output": {
+			dir: 'output',
+			serverDir: 'output/server',
+			publicDir: 'output/public'
+		}
+	},
+  generate:{
+    fallback:'error404.html'
+  }
 })
---
+++ pages/error404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>404</h2>
+    <p>Page Not Found</p>
+  </div>
+</template>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
