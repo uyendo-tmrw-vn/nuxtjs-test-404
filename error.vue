be74b2225b32dd101732aa1847e20c9dac47003f
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-03-03 11:29:02 +0700

    test

--- error.vue
+++ error.vue
@@ -1,21 +1,16 @@
 <template>
-  <NuxtLayout name="error">
-    <div class="mt-7 max-w-sm mx-auto text-center card">
-      <p class="mt-7 text-6xl">| Ooops.</p>
-      <button class="btn my-7">Go Home...</button>
-    </div>
-  </NuxtLayout>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{error.statusCode}}</p>
+    <p class="mt-7 text-6xl">Ooops.</p>
+    <p class="mt-7">{{error.message}}</p>
+    <button class="btn my-7" @click="handleError">Go Home...</button>
+  </div>
 </template>
-<script>
-export default {
-  setup() {
-    if(process.server) {
-      const nuxtApp = useNuxtApp()
-      const error = new Error()
-      error.statusCode = 404
-      // nuxtApp.ssrContext.error = error
-      nuxtApp.ssrContext.nuxt.error = error;
-    }
-  },
-}
+
+<script setup>
+  defineProps({
+    error: Object
+  })
+
+  const handleError = () => clearError({ redirect: '/' })
 </script>
